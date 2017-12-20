79b44d0cfd3bf866785811be409b6476d8de150f
Aarni Koskela <akx@iki.fi>
2017-12-21 00:25:39 +0200

    FASHION STYLEY

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './style.css';
 
 const root = document.createElement('main');
 document.body.appendChild(root);
