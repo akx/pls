f7c827c0a36d7d708af45d1b1bfafaf4c70b1a77
Aarni Koskela <akx@iki.fi>
2019-06-19 18:15:52 +0300

    Componentize PlaylistEntriesTable

--- src/components/types.tsx
+++ src/components/types.tsx
@@ -2,3 +2,7 @@ export interface FilterProps {
   filters: { [key: string]: string };
   setFilterValue: (key: string, value: string) => void;
 }
+
+export interface NumberLimits {
+  [key: string]: [number, number];
+}
