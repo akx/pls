d897bb6c4afefbd7339837844dff92c9e3709b4d
Aarni Koskela <akx@iki.fi>
2024-04-22 16:27:58 +0300

    Upgrade dependencies, build from GHA (#39)
    * Upgrade dependencies
    
    * Deploy from GHA

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 /* eslint-env node */
-import {ConfigEnv, defineConfig} from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { ConfigEnv, defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default ({mode}: ConfigEnv) => {
+export default () => {
   return defineConfig({
-    plugins: [reactRefresh()],
+    plugins: [react()],
     server: {
       port: 4000,
     },
-    base: "https://akx.github.io/pls/",
+    base: 'https://akx.github.io/pls/',
   });
-}
+};
